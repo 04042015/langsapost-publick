a6f184fa5507416fa9d028df7672ba58f8b6741a
04042015 <m.iggopramulia14@gmail.com>
2025-07-18 18:39:36 +0700

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,13 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute: "class" | "id" | "data-theme";
+  defaultTheme?: string;
+  enableSystem?: boolean;
+};
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
